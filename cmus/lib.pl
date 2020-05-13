94dc24aeddd143e3dc028a357e21b323155a27f4
AaronUlloa <aaronmars77@gmail.com>
2020-05-12 20:48:44 -0600

    Nuevas configuraciones

---
+++ cmus/lib.pl
@@ -0,0 +1,13 @@
+/home/Caronte-/Música/PhilipSayce/Philip Sayce - As The Years Go Passing By - Blues.mp3
+/home/Caronte-/Música/PhilipSayce/Philip Sayce - Driving South Testify Love Is A Powerful Thing - Blues.mp3
+/home/Caronte-/Música/PhilipSayce/Philip Sayce - I'M GOING HOME - Blues.mp3
+/home/Caronte-/Música/PhilipSayce/Philip Sayce - Who Knows If 6 Was 9 - Blues.mp3
+/home/Caronte-/Música/PhilipSayce/Philip Sayce - Steamroller Love Is A Powerful Thing - Blues.mp3
+/home/Caronte-/Música/ElectPro/PROGRAMMING - Hacking music vol.1.mp3
+/home/Caronte-/Música/SubUrban/Sub Urban - Cradles - Experimental.mp3
+/home/Caronte-/Música/SubUrban/Sub Urban - Freak - Experimental.mp3
+/home/Caronte-/Música/TheScore/The Score - Glory - Post-Rock.mp3
+/home/Caronte-/Música/TheScore/The Score - Legend - Post-Rock.mp3
+/home/Caronte-/Música/TheScore/The Score - Revolution - Post-Rock.mp3
+/home/Caronte-/Música/TheScore/The Score - Stronger - Post-Rock.mp3
+/home/Caronte-/Música/TheScore/The Score - Unstoppable - Post-Rock.mp3
